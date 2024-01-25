07dd3465a33d9b8447d6311c7d88b93775be7833
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-01-25 21:02:34 +0900

    Updates viewcontroller, storyboard, contents

--- Challenge3/Challenge3/ViewController.swift
+++ Challenge3/Challenge3/ViewController.swift
@@ -8,18 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var titleLabelL: UILabel!
-    @IBOutlet weak var mainImage: UIImage!
-    @IBOutlet weak var mainText: UITextView!
-    @IBOutlet weak var emailTextField: UITextField!
-    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet var buttons: [UIButton]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(adjustButtonDynamicType),
+                                               name: UIContentSizeCategory.didChangeNotification,
+                                               object: nil)
     }
 
-
+    @objc func adjustButtonDynamicType() {
+        buttons.forEach{button in
+            button.titleLabel?.adjustsFontForContentSizeCategory = true
+        }
+    }
 }
+
 
