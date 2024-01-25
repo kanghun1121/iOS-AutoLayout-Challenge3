111ff46fb5b54e3418df480df749de718d655594
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-01-25 16:37:07 +0900

    update viewcontroller

--- Challenge3/Challenge3/ViewController.swift
+++ Challenge3/Challenge3/ViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabelL: UILabel!
+    @IBOutlet weak var mainImage: UIImage!
+    @IBOutlet weak var mainText: UITextView!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
